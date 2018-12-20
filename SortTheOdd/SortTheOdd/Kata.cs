445e1880dac5aceadea7da0ae8dc10b3b3025c2f
阿爽 <krisonepiece@gmail.com>
2018-12-21 01:21:07 +0800

    add SortTheOdd

---
+++ SortTheOdd/SortTheOdd/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortTheOdd
+{
+    public class Kata
+    {
+        public static int[] SortArray(int[] array)
+        {
+            var oddStack = new Queue(array.Where(x => isOdd(x)).OrderBy(x => x).ToList());
+            for(var i = 0; i < array.Length; i++)
+                if (isOdd(array[i]))
+                    array[i] = (int)oddStack.Dequeue();
+            return array;
+        }
+        private static bool isOdd(int x)
+        {
+            return x % 2 == 1;
+        }
+    }
+}
---
+++ SortTheOdd/SortTheOddTest/KataTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortTheOdd;
+
+namespace SortTheOddTest
+{
+    [TestClass]
+    public class KataTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expect = new int[] { 1, 3, 2, 8, 5, 4 };
+            var actual = Kata.SortArray(new int[] { 5, 3, 2, 8, 1, 4 });
+            CollectionAssert.AreEqual(expect, actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var expect = new int[] { 1, 3, 2, 8, 5, 4 };
+            var actual = Kata.SortArray(new int[] { 5, 3, 2, 8, 1, 4 });
+            CollectionAssert.AreEqual(expect, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var expect = new int[] { };
+            var actual = Kata.SortArray(new int[] { });
+            CollectionAssert.AreEqual(expect, actual);
+        }
+    }
+}
