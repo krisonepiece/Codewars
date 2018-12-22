fddfee53333e1d032370fb13867908d0085849b4
阿爽 <krisonepiece@gmail.com>
2018-12-23 00:46:42 +0800

    add CarParkEscape

---
+++ CarParkEscape/CarParkEscape/Kata.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarParkEscape
+{
+    public class Kata
+    {
+        private const int CAR = 2;
+        private const int STAIR = 1;
+        private List<string> route = new List<string>();
+        private int level = 0;
+        private int space = 0;
+        private int curLevel = 0;
+        private int curSpace = 0;
+
+        public string[] escape(int[,] carpark)
+        {
+            level = carpark.GetLength(0);
+            space = carpark.GetLength(1);
+
+            findCar(carpark);
+            while(curLevel != level - 1)
+            {
+                findStair(carpark);
+                goDown(carpark);
+            }
+            findExit();
+
+            return route.ToArray();
+        }
+
+        private void goRight(int step)
+        {
+            route.Add("R" + step);
+        }
+        private void goLeft(int step)
+        {
+            route.Add("L" + step);
+        }
+        private void goDown(int[,] carpark)
+        {
+            var count = 0;
+            while(carpark[curLevel, curSpace] == STAIR)
+            {
+                count++;
+                curLevel++;
+            }
+            route.Add("D" + count);
+        }
+        private void findCar(int[,] carpark)
+        {
+            for(var i = 0; i < level; i++)
+            {
+                for (var j = 0; j < space; j++)
+                {
+                    if(carpark[i, j] == CAR)
+                    {
+                        curLevel = i;
+                        curSpace = j;
+                        return;
+                    }
+                }
+            }
+        }
+        private void findStair(int[,] carpark)
+        {
+            var stair = 0;
+            for(stair = 0; stair < space; stair++)
+            {
+                if(carpark[curLevel, stair] == STAIR)
+                {
+                    var count = stair - curSpace;
+                    if (count > 0)
+                        goRight(count);
+                    else
+                        goLeft(-count);
+                    break;
+                }
+            }
+            curSpace = stair;
+        }
+        private void findExit()
+        {
+            var count = space - 1 - curSpace;
+            if (count > 0)
+                goRight(count);
+        }
+    }
+}
---
+++ CarParkEscape/CarParkEscapeTest/KataTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using CarParkEscape;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarParkEscapeTest
+{
+    [TestClass]
+    public class KataTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expect = new string[] { "L4", "D1", "R4" };
+            var actual = new Kata().escape(new int[,] {   { 1, 0, 0, 0, 2 }, 
+                                                    { 0, 0, 0, 0, 0 }});
+            CollectionAssert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var expect = new string[] { "R3", "D2", "R1" };
+            var actual = new Kata().escape(new int[,] {   { 2, 0, 0, 1, 0 },
+                                                    { 0, 0, 0, 1, 0 },
+                                                    { 0, 0, 0, 0, 0 }});
+            CollectionAssert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var expect = new string[] { "R3", "D3" };
+            var actual = new Kata().escape(new int[,] {   { 0, 2, 0, 0, 1 },
+                                                    { 0, 0, 0, 0, 1 },
+                                                    { 0, 0, 0, 0, 1 },
+                                                    { 0, 0, 0, 0, 0 }});
+            CollectionAssert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var expect = new string[] { "L4", "D1", "R4", "D1", "L4", "D1", "R4" };
+            var actual = new Kata().escape(new int[,] {   { 1, 0, 0, 0, 2 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 1, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 0 }});
+            CollectionAssert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var expect = new string[] { };
+            var actual = new Kata().escape(new int[,] { { 0, 0, 0, 0, 2 } });
+            CollectionAssert.AreEqual(expect, actual);
+        }
+    }
+}
