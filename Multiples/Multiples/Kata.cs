2813a3924b65a33a9268f018f51589fa41415226
蘇王奕翔 <krisonepiece@gmail.com>
2018-12-25 18:19:59 +0800

    add Multiples

---
+++ Multiples/Multiples/Kata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples
+{
+    public static class Kata
+    {
+        public static int Solution(int value)
+        {
+            var sum = 0;
+            for (var i = 1; i < value; i++)
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            return sum;
+        }
+    }
+}
