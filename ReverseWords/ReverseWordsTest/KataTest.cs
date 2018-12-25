d61d5c718ed38b45e1dea587f575fea90837c411
阿爽 <krisonepiece@gmail.com>
2018-12-25 22:50:31 +0800

    add ReverseWords

---
+++ ReverseWords/ReverseWordsTest/KataTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReverseWords;
+
+namespace ReverseWordsTest
+{
+    [TestClass]
+    public class KataTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var expect = "sihT si na !elpmaxe";
+            var actual = Kata.ReverseWords("This is an example!");
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
---
+++ ReverseWords/ReverseWordsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReverseWordsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReverseWordsTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bb512e2b-52cd-4d51-8569-26f84bb3be41")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
